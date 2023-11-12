3f2bde6f83d46b1828e7606aeb10e2966c2c7da3
OMAR ALOUFI <63634434+omarfhad@users.noreply.github.com>
2023-11-12 04:59:44 +0300

    Update st_predict_part.py

--- st_predict_part.py
+++ st_predict_part.py
@@ -23,7 +23,14 @@ new_s = pd.read_excel(excel_file,
 grad_s = pd.read_excel(excel_file,
                                 sheet_name=sheet_name3)
 
+# Load the trained model
+model_filename = 'banglore_home_prices_model.pickle'
+with open(model_filename, 'rb') as file:
+    model = pickle.load(file)
 
+# Load the dataset used for training
+df_filename = 'bhp.csv'
+df = pd.read_csv(df_filename)
 
 
 def section1():
